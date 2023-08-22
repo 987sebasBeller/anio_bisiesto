de78160581ba097134db49b05be775a9680f3cff
sebastianBeller <sebastianbeller76@gmail.com>
2023-08-22 09:36:41 -0400

    no es bisiesto si  es divisible por 100 pero no por 400

--- src/esBisiesto.spec.js
+++ src/esBisiesto.spec.js
@@ -3,57 +3,28 @@
 import Year from "./year";
 describe("Anio Bisiesto",()=>{
     it("es bisiesto si es divisible por 400",()=>{
-        const year=new Year(2000);
-        expect(year.esBisiesto()).toEqual(true);
+        //const year=new Year(2000);
+        expect(anioBisiesto(2000)).toEqual(true);
     });
     it("no es bisiesto si no es divisible por 400",()=>{
-        const year=new Year(2001);
-        expect(year.esBisiesto()).toEqual(false);
+        // // arrange -> configurar
+        // const year=new Year(2001);// arrange configurar
+        // const bisiesto=year.esBisiesto();//act ejecutar
+        // expect(bisiesto).toEqual(false);// assert verificar
+
+        expect(anioBisiesto(2001)).toEqual(false);
     });
-    // it("El 400 deberia ser considerado anio bisiesto",()=>{
-    //     expect(esBisiesto(400)).toEqual(true);
-    // });
-    // it("El 2000  deberia ser considerado anio bisiesto",()=>{
-    //     expect(esBisiesto(2000)).toEqual(true);
-    // });
-    // it("El 100 no deberia ser considerado anio bisiesto",()=>{
-    //     expect(esBisiesto(100)).toEqual(false);
-    // });
-    // it("El 1700 no deberia ser considerado anio bisiesto",()=>{
-    //     expect(esBisiesto(1700)).toEqual(false);
-    // });
-    // it("El 1800 no deberia ser considerado anio bisiesto",()=>{
-    //     expect(esBisiesto(1800)).toEqual(false);
-    // });
-    // it("El 1900 no deberia ser considerado anio bisiesto",()=>{
-    //     expect(esBisiesto(1900)).toEqual(false);
-    // });
-    // it("El 2100 no deberia ser considerado anio bisiesto",()=>{
-    //     expect(esBisiesto(2100)).toEqual(false);
-    // });
-    // it("El 4 deberia ser considerado anio bisiesto",()=>{
-    //     expect(esBisiesto(4)).toEqual(true);
-    // });
-    // it("El 2008 deberia ser considerado anio bisiesto",()=>{
-    //     expect(esBisiesto(2008)).toEqual(true);
-    // });
-    // it("El 2012 deberia ser considerado anio bisiesto",()=>{
-    //     expect(esBisiesto(2012)).toEqual(true);
-    // });
-    //     it("El 2012 deberia ser considerado anio bisiesto",()=>{
-    //     expect(esBisiesto(2012)).toEqual(true);
-    // });
-    // it("El 2016 deberia ser considerado anio bisiesto",()=>{
-    //     expect(esBisiesto(2016)).toEqual(true);
-    // });
-    // it("El 2017 no deberia ser considerado anio bisiesto",()=>{
-    //     expect(esBisiesto(2017)).toEqual(false);
-    // });
-    // it("El 2018 no deberia ser considerado anio bisiesto",()=>{
-    //     expect(esBisiesto(2018)).toEqual(false);
-    // });
-    // it("El 2019 no deberia ser considerado anio bisiesto",()=>{
-    //     expect(esBisiesto(2019)).toEqual(false);
-    // });
+    //como ya cumple que con la anterior lo dejamos (IMPORTANTE PONER UN BUEN MENSAJE EN EL IT)
+    it("no es bisiesto si  es divisible por 100 pero no por 400",()=>{//en el it ponermos la regla del negocio si o si 
+        expect(anioBisiesto(1700)).toEqual(false);
+    });
+
+
+
+
+    function anioBisiesto(anio){
+        return new Year(anio).esBisiesto();
+
+    }
     
 });
